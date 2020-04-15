c7abe1d8ea0319372823f96d55d9376e137f221a
kfonseca <karstenfonseca@gmail.com>
2020-04-15 16:29:18 -0700

    Begin merging of files

--- motion_detector.py
+++ motion_detector.py
@@ -1,99 +1,103 @@
-# import the necessary packages
 import imutils
 import cv2
 import json
+import yolo
 
-# Load details
-config_file = 'config.json'
-with open(config_file) as f:
-	config_dict = json.load(f)
+def motion_detector(ip_cams, show_frames=False):
 
-# Load links to ip cams
-ip_cams = config_dict['ip_cams']
+	cap = cv2.VideoCapture(str(ip_cams[1]))
 
-# if the video argument is None, then we are reading from webcam
-if ip_cams is None:
-	exit()
+	# initialize the frame in the video stream
+	avg = None
+	min_motion_frames = 5
+	motionCounter = 0
+	min_area = 500
+	delta_thresh = 5
 
-vs = cv2.VideoCapture(str(ip_cams[1]))
+	# loop over the frames of the video
+	while True:
+		# grab current frame
+		status, frame = cap.read()
 
+		# if frame not available, exit
+		if frame is None:
+			break
 
-# initialize the frame in the video stream
-avg = None
-min_motion_frames = 5
-motionCounter = 0
-min_area = 500
-delta_thresh = 5
+		# Assume no motion
+		motion = False
 
-# loop over the frames of the video
-while True:
-	# grab the current frame
-	status, frame = vs.read()
+		# resize the frame, convert it to grayscale, and blur it
+		frame = imutils.resize(frame, width=500)
+		frame_gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+		frame_gray = cv2.GaussianBlur(frame_gray, (21, 21), 0)
 
-	# if frame not available, exit
-	if frame is None:
-		break
-
-	# Assume no motion
-	motion = False
-
-	# resize the frame, convert it to grayscale, and blur it
-	frame = imutils.resize(frame, width=500)
-	frame_gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
-	frame_gray = cv2.GaussianBlur(frame_gray, (21, 21), 0)
-
-	# if the average frame is None, initialize it
-	if avg is None:
-		avg = frame_gray.copy().astype("float")
-		continue
-
-	# Do weighted average between current & previous frame
-	# Then compute difference between curr and average frame
-	cv2.accumulateWeighted(frame_gray, avg, 0.5)
-	frameDelta = cv2.absdiff(frame_gray, cv2.convertScaleAbs(avg))
-
-	thresh = cv2.threshold(frameDelta, delta_thresh, 255, cv2.THRESH_BINARY)[1]
-
-	# dilate the thresholded image to fill in holes, then find contours on thresholded image
-	thresh = cv2.dilate(thresh, None, iterations=2)
-	cnts = cv2.findContours(thresh.copy(), cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
-	cnts = imutils.grab_contours(cnts)
-
-	# loop over the contours
-	for c in cnts:
-		# if the contour is too small, ignore it
-		if cv2.contourArea(c) < min_area:
+		# if the average frame is None, initialize it
+		if avg is None:
+			avg = frame_gray.copy().astype("float")
 			continue
 
-		# compute the bounding box for the contour, draw it on the frame,
-		# ---- TODO Not needed if not viewing
-		(x, y, w, h) = cv2.boundingRect(c)
-		cv2.rectangle(frame, (x, y), (x + w, y + h), (0, 255, 0), 2)
-		# ---- TODO Not needed if not viewing
-		motion = True
+		# Do weighted average between current & previous frame
+		# Then compute difference between curr and average frame
+		cv2.accumulateWeighted(frame_gray, avg, 0.5)
+		frameDelta = cv2.absdiff(frame_gray, cv2.convertScaleAbs(avg))
 
-	if motion:
-		motionCounter += 1
-	else:
-		motionCounter = 0
+		thresh = cv2.threshold(frameDelta, delta_thresh, 255, cv2.THRESH_BINARY)[1]
 
-	if motionCounter >= min_motion_frames:
-		print('Start Detection')
-		# Do YOLO detection
-		motionCounter = 0
+		# dilate the thresholded image to fill in holes, then find contours on thresholded image
+		thresh = cv2.dilate(thresh, None, iterations=2)
+		cnts = cv2.findContours(thresh.copy(), cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+		cnts = imutils.grab_contours(cnts)
+
+		# loop over the contours
+		for c in cnts:
+			# if the contour is too small, ignore it
+			if cv2.contourArea(c) < min_area:
+				continue
+			
+			motion = True
+
+			# compute the bounding box for the contour, draw it on the frame,
+			if show_frames:
+				(x, y, w, h) = cv2.boundingRect(c)
+				cv2.rectangle(frame, (x, y), (x + w, y + h), (0, 255, 0), 2)
+
+		if motion:
+			motionCounter += 1
+		else:
+			motionCounter = 0
+
+		if motionCounter >= min_motion_frames:
+			print('Start Detection')
+			# Do YOLO detection
+			motionCounter = 0
 
 
-	# ---- TODO Not needed if not viewing
-	# show the frame and record if the user presses a key
-	cv2.imshow("Security Feed", frame)
-	cv2.imshow("Thresh", thresh)
-	cv2.imshow("Frame Delta", frameDelta)
-	key = cv2.waitKey(1) & 0xFF
-	# if the `q` key is pressed, break from the loop
-	if key == ord("q"):
-		break
-	# ---- TODO Not needed if not viewing
+		# show the frame and record if the user presses a key
+		if show_frames:
+			cv2.imshow("Security Feed", frame)
+			cv2.imshow("Thresh", thresh)
+			cv2.imshow("Frame Delta", frameDelta)
+		
+		# if the `q` key is pressed, break from the loop
+		key = cv2.waitKey(1) & 0xFF
+		if key == ord("q"):
+			break
 
-# cleanup the camera and close any open windows
-vs.release()
-cv2.destroyAllWindows()
+	# cleanup the camera and close any open windows
+	cap.release()
+	cv2.destroyAllWindows()
+
+
+
+if __name__ == '__main__':
+
+	# Load details
+	with open('config.json') as f:
+		config_dict = json.load(f)
+
+	# Load links to ip cams
+	ip_cams = config_dict['ip_cams']
+	if ip_cams is None:
+		exit()
+	
+	motion_detector(ip_cams, True)
--- yolo.py
+++ yolo.py
@@ -3,12 +3,15 @@ import sys
 import numpy as np
 import os.path
 import json
+import time
 from datetime import datetime
 
 # Initialize the parameters
+classes = None
 confThreshold = 0.8  # Confidence threshold
 nmsThreshold = 0.4   # Non-maximum suppression threshold
 inpWidth = inpHeight = 320       # Height/Width of network's input image
+frame = None
 
 # Get the names of the output layers
 def getOutputsNames(net):
@@ -27,7 +30,7 @@ def drawPred(classId, conf, left, top, right, bottom):
     # Get the label for the class name and its confidence
     if classes:
         assert(classId < len(classes))
-        label = '%s:%s' % (classIds[i], label)
+        label = '%s:%s' % (classes[classId], label)
 
     #Display the label at the top of the bounding box
     labelSize, baseLine = cv.getTextSize(label, cv.FONT_HERSHEY_SIMPLEX, 0.5, 1)
@@ -36,7 +39,7 @@ def drawPred(classId, conf, left, top, right, bottom):
     cv.putText(frame, label, (left, top), cv.FONT_HERSHEY_SIMPLEX, 0.75, (0,0,0), 1)
 
 # Remove the bounding boxes with low confidence using non-maxima suppression
-def postprocess(frame, outs):
+def postprocess(frame, outs, show_frame=False, store_image = False):
     frameHeight = frame.shape[0]
     frameWidth = frame.shape[1]
 
@@ -66,19 +69,19 @@ def postprocess(frame, outs):
     for i in indices:
         i = i[0]
         # Skip classes that aren't people
-        if classIds[i] != 0:
-            continue
+        # if classIds[i] != 0:
+        #     continue
         box = boxes[i]
         left = box[0]
         top = box[1]
         width = box[2]
         height = box[3]
-        drawPred(classIds[i], confidences[i], left, top, left + width, top + height)
-        
-        class_id = classes[classIds[i]]
-        dimensions = (top, top + height, left, left+width)
-        write_image(frame, class_id, dimensions)
-
+        if store_image:
+            class_id = classes[classIds[i]]
+            dimensions = (top, top + height, left, left+width)
+            write_image(frame, class_id, dimensions)
+        if show_frame:
+            drawPred(classIds[i], confidences[i], left, top, left + width, top + height)
 
 def write_image(frame, class_id, dimensions):
     fileName = str(class_id) + '_' + str(datetime.now()) + '.png'
@@ -86,33 +89,28 @@ def write_image(frame, class_id, dimensions):
     outFile = frame[top:bottom, left:right]
     cv.imwrite(fileName, outFile)
 
-
-if __name__ == '__main__':
-    
-    # Load details
-    with open('config.json') as f:
-        config_dict = json.load(f)
-
-    # Load name of classes
-    classes = config_dict['coco_classes']
-    # Load links to ip cams
-    ip_cams = config_dict['ip_cams']
-
+def run_yolo(cap, coco_classes, duration, show_frame=False, store_image=False):
+    global frame, classes
     # Give the configuration and weight files for the model and load the network using them.
     modelConfiguration = "yolov3.cfg"
     modelWeights = "yolov3.weights"
+    classes = coco_classes
 
     net = cv.dnn.readNetFromDarknet(modelConfiguration, modelWeights)
     net.setPreferableBackend(cv.dnn.DNN_BACKEND_OPENCV)
     net.setPreferableTarget(cv.dnn.DNN_TARGET_CPU)
 
-    # Process inputs
-    cv.namedWindow("Stream", cv.WINDOW_NORMAL)
-    cap = cv.VideoCapture(str(ip_cams[0]))
     if not cap.isOpened():
-        raise IOError("Couldn't open webcam or video")
+        raise IOError("Couldn't open video")
+    
+    if show_frame:
+        cv.namedWindow("Yolo", cv.WINDOW_NORMAL)
 
     while cv.waitKey(1) < 0:
+
+        if duration:
+            if time.time() > duration:
+                break
 
         # Grab frame from video
         for _ in range(0, 24):
@@ -124,10 +122,8 @@ if __name__ == '__main__':
         # frame = frame[300:1080, 200:1920]
         
         # Stop the program if reached end of video
-        if not hasFrame:
-            cap.release()
-            cv2.destroyAllWindows()
-            exit()
+        if frame is None:
+            break
 
         # Create a 4D blob from a frame.
         blob = cv.dnn.blobFromImage(frame, 1/255, (inpWidth, inpHeight), [0,0,0], 1, crop=False)
@@ -139,7 +135,7 @@ if __name__ == '__main__':
         outs = net.forward(getOutputsNames(net))
 
         # Remove the bounding boxes with low confidence
-        postprocess(frame, outs)
+        postprocess(frame, outs, show_frame, store_image)
 
         # Put efficiency information. The function getPerfProfile returns the overall time for inference(t) and the timings for each of the layers(in layersTimes)
         # t, _ = net.getPerfProfile()
@@ -147,4 +143,21 @@ if __name__ == '__main__':
         # cv.putText(frame, label, (0, 15), cv.FONT_HERSHEY_SIMPLEX, 0.5, (0, 0, 255))
         # print(label)
 
-        cv.imshow("Stream", frame)
+        if show_frame:
+            cv.imshow("Yolo", frame)
+
+if __name__ == '__main__':
+
+    # Load details
+    with open('config.json') as f:
+        config_dict = json.load(f)
+
+    # Load links to ip cams
+    ip_cams = config_dict['ip_cams']
+
+    # Process inputs
+    cap = cv.VideoCapture(str(ip_cams[0]))
+
+    timeout = time.time() + 10*1
+
+    run_yolo(cap, config_dict['coco_classes'], timeout, True, False)
