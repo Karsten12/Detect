6b30d1c9f896dba28a8fd919b69a34db45cfda52
kfonseca <karstenfonseca@gmail.com>
2020-04-24 18:45:22 -0700

    fixed script, added back mask

--- output/delete_pics.sh
+++ output/delete_pics.sh
@@ -1,2 +1,11 @@
 # Script to delete all captured frames
-find . -type f -name '*.png' -exec rm {} +
+DESIRED_DIR="output"
+CURRENT_DIR=${PWD##*/} 
+
+if [ "$DESIRED_DIR" == "$CURRENT_DIR" ]
+then
+    echo Deleting pics
+    # find . -type f -name '*.png' -exec rm {} +
+else
+    echo Incorrect directory
+fi
